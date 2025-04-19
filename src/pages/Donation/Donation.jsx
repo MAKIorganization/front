ba7f4c5844818d01dd5229fb7dc0f7df17e64947
Tilek Orozobekov <tilekusupbekov678@gmail.com>
2025-04-19 14:42:36 +0600

    added donation page to the website

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,7 +1,29 @@
+import optimaImage from "../../assets/Optima_bank.jpg"
+import mbankImage from "../../assets/mbank-line.jpg"
+import styles from "./Donation.module.css"
+
 function Donation(){
     return(
         <>
-            Donation page
+            <p className={styles.volunteerDonationPrompt}>
+                <span>Важно!</span> Маки - некоммерческий проект. Мы не занимаемся продажей товаров, 
+                зато сохраняем природу и заботимся о здоровье человечества. Если хочешь, 
+                ты можешь поддержать нас финансово!
+            </p>
+            <h1>Способы поддержки</h1>
+            <h4>Перевод на счета</h4>
+            <div className={styles.bankRequisites}>
+                <div className={styles.optimaBank}>
+                    <img src={optimaImage} alt="" />
+                    <h3>41698989898989898</h3>
+                    <h3>Инициалы: ББУ</h3>
+                </div>
+                <div className={styles.mBank}>
+                    <img src={mbankImage} alt="" />
+                    <h3>41698989898989898</h3>
+                    <h3>Инициалы: ИИР</h3>
+                </div>
+            </div>
         </>
     )
 }
